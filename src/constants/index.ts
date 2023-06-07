cd8c9122d60c47ba992541f18fdadd577dcc5eb2
pedanmax <m.pedan2017@gmail.com>
2023-06-07 12:14:52 +0300

    feat:add responsive to get page

--- src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export const GETREQUESTWAY = 'https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=6';
-
-export const POSTREQUESTWAY = 'https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=6';
